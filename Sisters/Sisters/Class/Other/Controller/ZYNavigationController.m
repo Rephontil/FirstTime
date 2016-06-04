b1ccc7c0d87241bab1fe4196db333d10354b0fd7
周勇 <pm@raphaelyuantekimacbook-pro.local>
2016-06-04 23:05:25 +0800

    关注模块处理完毕，修改了工程初始化代码的布局。

--- Sisters/Sisters/Class/Other/Controller/ZYNavigationController.m
+++ Sisters/Sisters/Class/Other/Controller/ZYNavigationController.m
@@ -10,16 +10,30 @@
 
 @interface ZYNavigationController ()
 
+
 @end
 
 @implementation ZYNavigationController
 
+
+//当第一次使用这个类的时候回执行一次
++ (void)initialize{
+    UINavigationBar* navigationBar = [UINavigationBar appearanceWhenContainedIn:[self class], nil];
+    [navigationBar setBackgroundImage:[UIImage imageNamed:@"navigationbarBackgroundWhite"] forBarMetrics:UIBarMetricsDefault];
+    ZYLogFun
+//    MainTagSubIcon
+//    navigationbarBackgroundWhite
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationBar.tintColor = [UIColor blackColor];
-    // Do any additional setup after loading the view.
     
-    
+//    通过下面的做法，以后不管是用ZYNavigationController创建的导航控制器还是用系统方法创建的导航视图控制器，它的UINavigationBar背景图都是一样的。因为直接使用了appearance.
+//    UINavigationBar* navigationBar = [UINavigationBar appearance];
+//    [navigationBar setBackgroundImage:[UIImage imageNamed:@"navigationbarBackgroundWhite"] forBarMetrics:UIBarMetricsDefault];
+//    UINavigationBar* navigationBar = [UINavigationBar appearanceWhenContainedIn:[self class], nil];
+//    [navigationBar setBackgroundImage:[UIImage imageNamed:@"navigationbarBackgroundWhite"] forBarMetrics:UIBarMetricsDefault];
 }
 
 
@@ -29,38 +43,34 @@
  ************CreateDate:Year:2016 Month:06 Date:04 ************
  ***************Creator:周勇**************************
  ****Function Describle:可以拦截所有push进来的控制器***
- ****************Remark:<#**************************************#>***
+ ****************Remark:将自定义的button当作视图控制器的返回Item***
  ****************************************************************/
-
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated
-{
+{    //判断一旦进入视图控制器的子控制器，也就是点击进去后，自定义的视图控制器返回的Item才会显示，以免干扰其他的图层
     if (self.childViewControllers.count>0) {
         UIButton* backBarButton = [UIButton buttonWithType:UIButtonTypeCustom];
         [backBarButton setTitle:@"返回" forState:UIControlStateNormal];
-        [backBarButton setImage:[UIImage imageNamed:@"navigationButtonReturnClick"] forState:UIControlStateHighlighted];
-        [backBarButton setImage:[UIImage imageNamed:@"navigationButtonReturn"] forState:UIControlStateNormal];
-        backBarButton.size = CGSizeMake(100, 30); //这样很low逼
-//        backBarButton.contentMode = UIViewContentModeBottomLeft;
+        [backBarButton setImage:[UIImage imageNamed:@"navigationButtonReturnClick"] forState:UIControlStateHighlighted]; //这个地方不是setBackgroundImage
+        [backBarButton setImage:[UIImage imageNamed:@"navigationButtonReturn"] forState:UIControlStateNormal];  //这个地方不是setBackgroundImage
+        backBarButton.size = CGSizeMake(100, 30); //这样很low逼,但是可以增大点击范围
+//        backBarButton.contentMode = UIViewContentModeBottomLeft; //作用于UIImageView,对button无效
 //        backBarButton.backgroundColor = [UIColor redColor]; //用于测试的color，不用了。
-        backBarButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
-        backBarButton.contentEdgeInsets = UIEdgeInsetsMake(0,-10, 0, 0);
-        
-        
+        backBarButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft; //让按钮内部所有内容左对齐
+        backBarButton.contentEdgeInsets = UIEdgeInsetsMake(0,-10, 0, 0); //让文字和作用区域左偏移
         /*
         backBarButton.backgroundColor = [UIColor redColor];
-        [backBarButton sizeToFit]; //让尺寸跟随里面的内容,这种做大很高大上的
+        [backBarButton sizeToFit]; //让尺寸跟随里面的内容,这种做大很高大上的,以后一直会用到
         */
         [backBarButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         [backBarButton setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
         [backBarButton addTarget:self action:@selector(back) forControlEvents:UIControlEventTouchUpInside];
         viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backBarButton];
         viewController.hidesBottomBarWhenPushed = YES;//当弹入一个新的视图控制器的时候，隐藏下面的标签栏tabbar
-        
     }
 //    我们这样做的目的是什么？
-    [super pushViewController:viewController animated:animated];
-
+    [super pushViewController:viewController animated:animated];//进入下一级视图控制器
 }
+
 
 - (void)back{
     [self popViewControllerAnimated:YES];
